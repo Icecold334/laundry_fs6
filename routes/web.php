fda0c81857d0f9d7f97cfd23ae9c45eff01e960a
Icecold334 <fauzanimam334@gmail.com>
2024-05-17 13:58:38 +0700

    add login view

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/auth', [AuthController::class, 'index']);
