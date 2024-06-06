2d4c83b10bc88a3f7e38d52ddb6f55a7c79b07d4
Icecold334 <fauzanimam334@gmail.com>
2024-06-06 13:31:11 +0700

    update logout confirm

--- resources/views/layout/admin/navbar.blade.php
+++ resources/views/layout/admin/navbar.blade.php
@@ -149,10 +149,10 @@
                     Settings
                 </a> --}}
                 <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                <button class="dropdown-item" id="logout">
                     <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                     Logout
-                </a>
+                </button>
             </div>
         </li>
 
