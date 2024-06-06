87b4d23af741418af2243ee4b3454aecf6b553a2
Icecold334 <fauzanimam334@gmail.com>
2024-06-06 18:39:42 +0700

    Dashboard Controller Update

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,39 +2,87 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Models\Orders;
-use App\Models\User;
 use Carbon\Carbon;
+use App\Models\User;
+use App\Models\Orders;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function index(Request $request)
     {
-        // Total Orders
-        $totalOrders = Orders::count();
-        $completedOrders = Orders::where('status', 4)->count();
+        if (Auth::user()->role == 1) {
+            // Hari ini
+            $today = Carbon::today();
 
-        // Total Pengguna
-        $totalUsers = User::where('role', 3)->count();
+            // Total Orders
+            $totalOrders = Orders::whereDate('created_at', $today)->count();
+            $completedOrders = Orders::where('status', 4)
+                ->whereDate('created_at', $today)
+                ->count();
 
-        // Circle Status Diagram
-        $statuses = [0, 1, 2, 3];
-        $orderStatus = [];
+            // Total Pendapatan
+            $totalRevenue = Orders::where('status', 4)
+                ->whereDate('created_at', $today)
+                ->sum('total');
 
-        foreach ($statuses as $status) {
-            $orderStatus[$status] = Orders::where('status', $status)->count();
+            // Total Karyawan
+            $totalEmployee = User::where('role', 2)->count();
+
+            // Total Pengguna
+            $totalUsers = User::where('role', 3)->count();
+
+            // Analisa Pesanan
+            $statuses = [0, 1, 2, 3];
+            $orderStatus = [];
+
+            foreach ($statuses as $status) {
+                $orderStatus[$status] = Orders::where('status', $status)
+                    ->whereDate('created_at', $today)
+                    ->count();
+            }
+
+            // Data
+            $data = [
+                'title' => 'Dashboard',
+                'totalOrders' => $totalOrders,
+                'completedOrders' => $completedOrders,
+                'totalUsers' => $totalUsers,
+                'totalEmployee' => $totalEmployee,
+                'orderStatus' => $orderStatus,
+                'totalRevenue' => 'Rp ' . number_format($totalRevenue, 0, ',', ','),
+            ];
+
+            return view('dashboard.index', $data);
+        } else if (Auth::user()->role == 2) {
+
+
+            // Total Orders
+            $totalOrders = Orders::count();
+            $completedOrders = Orders::where('status', 4)->count();
+
+            // Total Pengguna
+            $totalUsers = User::where('role', 3)->count();
+
+            // Circle Status Diagram
+            $statuses = [0, 1, 2, 3];
+            $orderStatus = [];
+
+            foreach ($statuses as $status) {
+                $orderStatus[$status] = Orders::where('status', $status)->count();
+            }
+
+            // Data
+            $data = [
+                'title' => 'Dashboard',
+                'totalOrders' => $totalOrders,
+                'completedOrders' => $completedOrders,
+                'totalUsers' => $totalUsers,
+                'orderStatus' => $orderStatus,
+            ];
+
+            return view('dashboard.index', $data);
         }
-
-        // Data
-        $data = [
-            'title' => 'Dashboard',
-            'totalOrders' => $totalOrders,
-            'completedOrders' => $completedOrders,
-            'totalUsers' => $totalUsers,
-            'orderStatus' => $orderStatus,
-        ];
-
-        return view('dashboard.index', $data);
     }
 }
