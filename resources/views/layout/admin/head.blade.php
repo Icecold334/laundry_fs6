5fb5ff9007d174428bd28518a560e586a4f3a7f9
Icecold334 <fauzanimam334@gmail.com>
2024-06-21 16:26:39 +0700

    disable alert

--- resources/views/layout/admin/head.blade.php
+++ resources/views/layout/admin/head.blade.php
@@ -36,5 +36,5 @@
     <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
-    @vite(['resources/js/app.js', 'resources/css/app.css'])
+    {{-- @vite(['resources/js/app.js', 'resources/css/app.css']) --}}
 </head>
